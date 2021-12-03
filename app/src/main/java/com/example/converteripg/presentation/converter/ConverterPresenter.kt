a5a3abdb9d7bffadd0eb4ce36275725adee4fcdc
татьяна <emuart155@gmail.com>
2021-12-03 19:28:19 +0300

    Некоторые исправления

--- app/src/main/java/com/example/converteripg/presentation/converter/ConverterPresenter.kt
+++ app/src/main/java/com/example/converteripg/presentation/converter/ConverterPresenter.kt
@@ -1,7 +1,7 @@
 package com.example.converteripg.presentation.converter
 
 import android.net.Uri
-import com.example.converteripg.data.converter.Converter
+import com.example.converteripg.data.converter.ConverterJpgToPng
 import com.example.converteripg.scheduler.Schedulers
 import com.example.converteripg.view.ConverterView
 import io.reactivex.rxjava3.disposables.CompositeDisposable
@@ -9,17 +9,22 @@ import io.reactivex.rxjava3.kotlin.plusAssign
 import moxy.MvpPresenter
 
 class ConverterPresenter(
-    private val converter: Converter,
+    private val converter: ConverterJpgToPng,
     private val schedulers: Schedulers,
 ) : MvpPresenter<ConverterView>() {
 
     private val disposables = CompositeDisposable()
 
-    fun convert(uri: Uri) {
-        //покажи картинку какую мы выбрали
-        viewState.showContent(uri)
-        //покажи экран загрузки
-        viewState.showLoading()
+    override fun onFirstViewAttach() {
+        super.onFirstViewAttach()
+        viewState.init()
+    }
+
+    fun startConvertingPressed(uri: Uri) {
+        viewState.showProgressBar()
+        viewState.signWaitingShow()
+        viewState.signGetStartedHide()
+        viewState.btnAbortConvertEnabled()
 
         disposables +=
             converter
@@ -27,14 +32,63 @@ class ConverterPresenter(
                 .subscribeOn(schedulers.background())
                 .observeOn(schedulers.main())
                 .subscribe(
-                    viewState::showContent,
-                    viewState::showError
-                )
+                    { t: Uri? ->
+                        if (t != null) {
+                            onConvertingSuccess(t)
+                        }
+                    },
+                    { e: Throwable? ->
+                        onConvertingError(e)
+                    })
     }
 
-    fun cancel() {
-        viewState.showContent(null)
+    /**
+     * Обработка успеха от источника
+     */
+    private fun onConvertingSuccess(uri: Uri) {
+        viewState.showConvertedImage(uri)
+        viewState.hideProgressBar()
+        viewState.btnAbortConvertDisabled()
+        viewState.signAbortConvertHide()
+        viewState.signWaitingHide()
+    }
+
+    /**
+     * Обработка ошибки от источника
+     */
+    private fun onConvertingError(e: Throwable?) {
+        viewState.showProgressBar()
+        viewState.hideProgressBar()
+        viewState.btnAbortConvertDisabled()
+        viewState.signWaitingHide()
+    }
+
+    /**
+     * Прерывание процесса конвертации изображения
+     * фактически отписывается от источника, и выводит зарезервированное изображение на экран
+     * попутно скрывает прогресс бар и деактивирует кнопку прерывания процесса конвертации
+     */
+    fun abortConvertImagePressed() {
+        viewState.hideProgressBar()
+        viewState.signWaitingHide()
+        viewState.btnAbortConvertDisabled()
+        viewState.signAbortConvertShow()
         disposables.clear()
+    }
+
+    /**
+     * Обрабатываемое изображение выбрано
+     * @param imageUri - Uri конвертируемого изображения (оригинала)
+     * отдает команду вывести выбранное изображение в виджет оригинала
+     * попутно активирует кнопку старта процесса конвертации изображения
+     * скрывает ненужные сигнумы и отображает картинку заглушку ожидания результата конвертации
+     */
+    fun originalImageSelected(imageUri: Uri) {
+        viewState.showOriginImage(imageUri)
+        viewState.btnStartConvertEnable()
+        viewState.signAbortConvertHide()
+        viewState.signGetStartedHide()
+        viewState.signWaitingShow()
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/converteripg/scheduler/SchedulersFactory.kt
+++ app/src/main/java/com/example/converteripg/scheduler/SchedulersFactory.kt
@@ -1,9 +1,5 @@
 package com.example.converteripg.scheduler
 
-import android.content.Context
-import com.example.converteripg.data.converter.Converter
-import com.example.converteripg.data.converter.ConverterImpl
-
 object SchedulersFactory {
 
     fun create(): Schedulers = DefaultSchedulers()
